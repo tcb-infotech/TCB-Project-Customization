d6614c6efa2eb17a9311f12a39309c6f4f9133b4
sahildhania069 <tcbsahil@gmail.com>
2025-01-27 13:18:13 +0530

    feat: Removed Operator Work column and a blank line after every entry

--- tcb_project_customization/tcb_project_customization/report/dpr/dpr.py
+++ tcb_project_customization/tcb_project_customization/report/dpr/dpr.py
@@ -1,5 +1,4 @@
-# Copyright (c) 2024, AjayRaj Mahiwal and contributors
-# For license information, please see license.txt
+
 
 import frappe
 from frappe import _
@@ -13,65 +12,6 @@ def execute(filters=None):
     
     return columns, data
 
-def get_columns():
-    return [
-        {
-            "label": _("ID"),
-            "fieldname": "id",
-            "fieldtype": "Link",
-            "options": "Direct Timesheet",
-            "width": 200
-        },
-        {
-            "label": _("Site (Project)"),
-            "fieldname": "site",
-            "fieldtype": "Link",
-            "options": "Project",
-            "width": 200
-        },
-        {
-            "label": _("Supervisor and Work"),
-            "fieldname": "supervisor_work",
-            "fieldtype": "Small Text",
-            "width": 300
-        },
-        {
-            "label": _("Operator and Work"),
-            "fieldname": "operator_work",
-            "fieldtype": "Small Text",
-            "width": 300
-        },
-        {
-            "label": _("Gang Leader and Work"),
-            "fieldname": "gang_leader_work",
-            "fieldtype": "Small Text",
-            "width": 300
-        },
-        {
-            "label": _("Watchmen"),
-            "fieldname": "watchmen",
-            "fieldtype": "Data",
-            "width": 300
-        },
-        {
-            "label": _("Labour Count"),
-            "fieldname": "labour",
-            "fieldtype": "Int",
-            "width": 100
-        },
-        {
-            "label": _("Total Staff"),
-            "fieldname": "total_staff",
-            "fieldtype": "Int",
-            "width": 100
-        },
-        {
-            "label": _("Remark"),
-            "fieldname": "remark",
-            "fieldtype": "Data",
-            "width": 500
-        }
-    ]
 
 def get_report_data(filters):
     # Fetch timesheets based on filters
@@ -114,37 +54,141 @@ def get_report_data(filters):
         """
         staff_details = frappe.db.sql(staff_query, ts.timesheet, as_dict=1)
 
-        # Group and process staff data
-        supervisors_work = []
-        operators_work = []
-        gang_leaders_work = []
+        # Separate staff details 
+        supervisors = []
+        supervisor_works = []
+        operators = []
+        # operator_works = []
+        gang_leaders = []
+        gang_leader_works = []
         watchmen = []
         labour_count = 0
 
         for staff in staff_details:
-            work = f"{staff.employee_name}: {staff.description}" if staff.description else staff.employee_name
             if staff.designation == "Supervisor":
-                supervisors_work.append(work)
+                supervisors.append(staff.employee_name)
+                if staff.description:
+                    supervisor_works.append(f"{staff.employee_name}: {staff.description}")
             elif staff.designation == "Operator":
-                operators_work.append(work)
+                operators.append(staff.employee_name)
+                # if staff.description:
+                #     operator_works.append(f"{staff.employee_name}: {staff.description}")
             elif staff.designation == "Gangleader":
-                gang_leaders_work.append(work)
+                gang_leaders.append(staff.employee_name)
+                if staff.description:
+                    gang_leader_works.append(f"{staff.employee_name}: {staff.description}")
                 labour_count += staff.labour_count or 0
             elif staff.designation == "Watchman":
                 watchmen.append(f"{staff.employee_name} ({staff.shift})")
 
-        # Prepare row data
+        # Prepare row data with full lists
         row = {
             "id": ts.timesheet,
             "site": ts.site,
-            "supervisor_work": " / ".join(supervisors_work),
-            "operator_work": " / ".join(operators_work),
-            "gang_leader_work": " / ".join(gang_leaders_work),
-            "watchmen": " / ".join(watchmen),
+            "supervisor_work": ", ".join(supervisors),
+            "supervisor_details": "; ".join(supervisor_works) if supervisor_works else " ",
+            "operator_work": ", ".join(operators) if operators else " ",
+            # "operator_details": "; ".join(operator_works) if operator_works else " ",
+            "gang_leader_work": ", ".join(gang_leaders) if gang_leaders else " ",
+            "gang_leader_details": "; ".join(gang_leader_works) if gang_leader_works else " ",
+            "watchmen": ", ".join(watchmen) if watchmen else " ",
             "labour": labour_count,
-            "total_staff": len(supervisors_work) + len(operators_work) + len(watchmen) + labour_count,
+            "total_staff": len(supervisors) + len(operators) + len(watchmen) + labour_count,
             "remark": frappe.db.get_value("Direct Timesheet", ts.timesheet, "remarks") or ""
         }
+        blankrow = {
+            "id": "",
+            "site": "",
+            "supervisor_work": "",
+            "supervisor_details": "",
+            "operator_work": "",
+            # "operator_details": "; ".join(operator_works) if operator_works else " ",
+            "gang_leader_work": "",
+            "gang_leader_details": "",
+            "watchmen": "",
+            "labour": "",
+            "total_staff": "",
+            "remark": ""
+        }
         data.append(row)
+        data.append(blankrow)
     
     return data
+
+def get_columns():
+    return [
+        {
+            "label": _("ID"),
+            "fieldname": "id",
+            "fieldtype": "Link",
+            "options": "Direct Timesheet",
+            "width": 200
+        },
+        {
+            "label": _("Site (Project)"),
+            "fieldname": "site",
+            "fieldtype": "Link",
+            "options": "Project",
+            "width": 200
+        },
+        {
+            "label": _("Supervisor"),
+            "fieldname": "supervisor_work",
+            "fieldtype": "Data",
+            "width": 150
+        },
+        {
+            "label": _("Supervisor Work"),
+            "fieldname": "supervisor_details",
+            "fieldtype": "Small Text",
+            "width": 250
+        },
+        {
+            "label": _("Operator"),
+            "fieldname": "operator_work",
+            "fieldtype": "Data",
+            "width": 150
+        },
+        # {
+        #     "label": _("Operator Work"),
+        #     "fieldname": "operator_details",
+        #     "fieldtype": "Small Text",
+        #     "width": 250
+        # },
+        {
+            "label": _("Gang Leader"),
+            "fieldname": "gang_leader_work",
+            "fieldtype": "Data",
+            "width": 150
+        },
+        {
+            "label": _("Gang Leader Work"),
+            "fieldname": "gang_leader_details",
+            "fieldtype": "Small Text",
+            "width": 250
+        },
+        {
+            "label": _("Watchmen"),
+            "fieldname": "watchmen",
+            "fieldtype": "Data",
+            "width": 200
+        },
+        {
+            "label": _("Labour Count"),
+            "fieldname": "labour",
+            "fieldtype": "Int",
+            "width": 100
+        },
+        {
+            "label": _("Total Staff"),
+            "fieldname": "total_staff",
+            "fieldtype": "Int",
+            "width": 100
+        },
+        {
+            "label": _("Remark"),
+            "fieldname": "remark",
+            "fieldtype": "Data",
+            "width": 500
+        }
+    ]
