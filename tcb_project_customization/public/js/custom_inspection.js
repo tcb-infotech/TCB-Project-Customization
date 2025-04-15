dfcb3ad530d6507b9c6732974e10087ea68dcf4a
sahildhania069 <tcbsahil@gmail.com>
2025-04-15 17:53:01 +0530

    fix: naming of inspection

--- tcb_project_customization/public/js/custom_inspection.js
+++ tcb_project_customization/public/js/custom_inspection.js
@@ -1,11 +1,19 @@
-// frappe.ui.form.on("Inspection",{
-//     refresh:function(frm){
-//         frm.fields_dict["inspector"].get_query =  function(doc,cdt,cdn){
-//             return{
-//                 filters:{
-//                     custom_inspection_inspector:1
-//                 }
-//             }
-//         }
-//     }
-// })
+frappe.ui.form.on("Inspection",{
+    onload: function(frm) {
+        const today = new Date();
+        let startYear, endYear;
+
+        if (today.getMonth() >= 3) { 
+            startYear = today.getFullYear();
+            endYear = startYear + 1;
+        } else { 
+            endYear = today.getFullYear();
+            startYear = endYear - 1;
+        }
+
+        const financialYear = `${startYear.toString().slice(-2)}-${endYear.toString().slice(-2)}`;
+        frm.set_value('custom_financial_year', financialYear);
+
+        
+    },
+})
