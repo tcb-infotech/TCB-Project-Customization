41e1334e0bc16af70f61d90bc63d392b3c28e56d
sahildhania069 <tcbsahil@gmail.com>
2025-04-15 10:06:01 +0530

    fix: Purchase Order required by date

--- tcb_project_customization/public/js/custom_po.js
+++ tcb_project_customization/public/js/custom_po.js
@@ -1,23 +1,23 @@
 frappe.ui.form.on('Purchase Order', {
-    // custom_required_by: function(frm) {
-    //     if (frm.doc.custom_required_by) {
-    //         let today = new Date();
+    custom_required_by: function(frm) {
+        if (frm.doc.custom_required_by) {
+            let today = new Date();
 
-    //         let days_to_add = frm.doc.custom_required_by;
+            let days_to_add = frm.doc.custom_required_by;
 
-    //         let newDate = new Date(today.setDate(today.getDate() + days_to_add));
+            let newDate = new Date(today.setDate(today.getDate() + days_to_add));
 
-    //         frm.set_value('schedule_date', newDate);
+            frm.set_value('schedule_date', newDate);
 
-    //         frm.set_df_property('schedule_date','read_only',1)
-    //         frm.refresh_field('schedule_date');
-    //     }
-    //     else{
-    //         // becomes editable if no days entered
-    //         frm.set_df_property('schedule_date','read_only',0)
-    //         frm.refresh_field('schedule_date')
-    //     }
-    // },
+            frm.set_df_property('schedule_date','read_only',1)
+            frm.refresh_field('schedule_date');
+        }
+        else{
+            // becomes editable if no days entered
+            frm.set_df_property('schedule_date','read_only',0)
+            frm.refresh_field('schedule_date')
+        }
+    },
     onload: function(frm) {
         const today = new Date();
         let startYear, endYear;
