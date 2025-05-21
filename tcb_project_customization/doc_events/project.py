44de2165e596f6be882daef0f0d7a28a49247dca
sahildhania069 <tcbsahil@gmail.com>
2025-05-21 11:54:24 +0530

    feat: new project creates a new warehouse

--- tcb_project_customization/doc_events/project.py
+++ tcb_project_customization/doc_events/project.py
@@ -45,3 +45,32 @@ def validate(self, *args, **kwargs):
 
     details_set = set()
     unique_details = []
+
+
+def after_insert(doc, method):
+
+# get_doc is used for two purposed, to fetch existing documents or create a new document
+# When you pass a dictionary of values with the 'doctype' key, 
+# it creates a new document instance but does not save it until you call .insert() or .save()
+    parent_warehouse_doc= frappe.get_doc({
+        'doctype':'Warehouse',
+        'warehouse_name':doc.project_name,
+        'custom_project':doc.name, 
+        'is_group': 1,
+    })
+
+    parent_warehouse_doc.insert()
+
+    
+    store_warehouse_doc= frappe.get_doc({
+        'doctype':'Warehouse',
+        'warehouse_name':f"{doc.project_name}-Store",
+        'custom_project':doc.name, 
+        'parent_warehouse': parent_warehouse_doc.name
+    })
+
+    store_warehouse_doc.insert()
+
+
+
+    frappe.msgprint("Warehouse Successfully Created")
