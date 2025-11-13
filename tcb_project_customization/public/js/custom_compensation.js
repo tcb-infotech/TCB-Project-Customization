46c213bec5ab0bc17ec60fc2987ad20dfa284f9f
sahildhania069 <tcbsahil@gmail.com>
2025-11-13 18:40:44 +0530

    feat: changes in compensation, added compensation enabling,, added new report - hrc purchase order analysis

--- tcb_project_customization/public/js/custom_compensation.js
+++ tcb_project_customization/public/js/custom_compensation.js
@@ -8,7 +8,7 @@ frappe.ui.form.on("Compensation Records",{
 function calculate_total(frm,cdt,cdn){
     let sum = 0
     frm.doc.compensation_areas.forEach((row)=>{
-        sum+= (row.land+row.rasta+row.foundation+row.stringing+row.crop)
+        sum+= (row.land+row.rasta+row.foundation+row.stringing+row.crop+row.compensation_enabling)
     })
     frm.set_value("paid_total_amount",sum)
 }
