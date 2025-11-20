d202c83d9df7b20f73c66edef9dd6cad4ae7167d
sahildhania069 <tcbsahil@gmail.com>
2025-11-20 13:45:31 +0530

    added fields in compensation records

--- tcb_project_customization/public/js/custom_compensation.js
+++ tcb_project_customization/public/js/custom_compensation.js
@@ -8,7 +8,7 @@ frappe.ui.form.on("Compensation Records",{
 function calculate_total(frm,cdt,cdn){
     let sum = 0
     frm.doc.compensation_areas.forEach((row)=>{
-        sum+= (row.land+row.rasta+row.foundation+row.stringing+row.crop+row.compensation_enabling)
+        sum+= (row.land+row.rasta+row.foundation+row.stringing+row.crop+row.compensation_enabling+row.erection+row.utr_no)
     })
     frm.set_value("paid_total_amount",sum)
 }
