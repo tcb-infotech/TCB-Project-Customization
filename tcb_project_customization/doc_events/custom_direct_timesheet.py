4398dafedf8ada063493c993f1159f2996c42ff8
sahildhania069 <tcbsahil@gmail.com>
2025-11-27 15:57:18 +0530

    fix: earlier the dpr was fixed on a single date,, now fixed

--- tcb_project_customization/doc_events/custom_direct_timesheet.py
+++ tcb_project_customization/doc_events/custom_direct_timesheet.py
@@ -2,7 +2,7 @@ import frappe
 from frappe.utils import nowdate
 
 def validate(doc,method=None):
-    today_dprs = frappe.db.get_all("Direct Timesheet",filters = {"start_date":nowdate(),"project":doc.project},fields=["name","project","project_name"])
-    if len(today_dprs)>1:
+    today_dprs = frappe.db.get_all("Direct Timesheet",filters = {"start_date":doc.start_date,"project":doc.project},fields=["name","project","project_name"])
+    if len(today_dprs)>0    :
         for entry in today_dprs:
-            frappe.throw(msg = "Existing DPR exists for <b>{0}</b> today - <b>{1}</b>".format(entry.project_name,entry.name))
+            frappe.throw(msg = "Existing DPR exists for <b>{0}</b> for this date - <b>{1}</b>".format(entry.project_name,entry.name))
