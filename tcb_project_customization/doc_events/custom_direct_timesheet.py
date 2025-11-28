393f604213794a7e82daa31813f95b5908c6b02b
sahildhania069 <tcbsahil@gmail.com>
2025-11-28 14:08:35 +0530

    fix: error that wont let you submit the dpr

--- tcb_project_customization/doc_events/custom_direct_timesheet.py
+++ tcb_project_customization/doc_events/custom_direct_timesheet.py
@@ -2,7 +2,7 @@ import frappe
 from frappe.utils import nowdate
 
 def validate(doc,method=None):
-    today_dprs = frappe.db.get_all("Direct Timesheet",filters = {"start_date":doc.start_date,"project":doc.project},fields=["name","project","project_name"])
-    if len(today_dprs)>0    :
+    today_dprs = frappe.db.get_all("Direct Timesheet",filters = {"start_date":doc.start_date,"project":doc.project,"name":["!=",doc.name]},fields=["name","project","project_name"])
+    if len(today_dprs)>0:
         for entry in today_dprs:
             frappe.throw(msg = "Existing DPR exists for <b>{0}</b> for this date - <b>{1}</b>".format(entry.project_name,entry.name))
